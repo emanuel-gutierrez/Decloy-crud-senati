9cc5a46a5aed3729225c2cbea926feca9af285c7
Emanuel <emanuelgutierrez595@gmail.com>
2025-04-11 10:51:11 -0500

    Desplegando mi app php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+$host="localhost";
+$user="root";
+$password="";
+$db="senati";
+$port=3306;
+
+$conexion = new mysqli($host, $user, $password, $db, $port);
+
+if($conexion->connect_error){
+    die("connection failed" . $conexion->connect_error);
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,35 @@
+ <?php include 'config.php'; ?>
+<style>
+    table{
+        ba
+    }
+ </style>   
+ <h1>Lista de Usuarios</h1>
+ 
+ <a href="create.php">Agregar Nuevo</a>
+
+ <table border="i">
+  <tr>
+  <td> id</td>
+  <td> name </td>
+  <td> lastname </td>
+  <td> Actions </td>
+
+</tr>
+
+<?php
+$result = $conexion->query("SELECT * FROM Usuario");
+
+while($row = $result->fetch_assoc()){
+ echo "
+    <tr>
+  <td> $row[id]</td>
+  <td> $row[name] </td>
+  <td> $row[lastname] </td>
+  <td><a href= 'a' </td>
+
+</tr> ";
+}
+
+?>
+</table>
